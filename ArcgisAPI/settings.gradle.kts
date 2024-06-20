78a82436189558a972b9d18a14d1f478885fe2c9
Eva <evacosta.if@gmail.com>
2024-06-19 23:37:00 -0300

    tarefa semana 6

---
+++ ArcgisAPI/settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven {
+            url = uri("https://esri.jfrog.io/artifactory/arcgis")
+        }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://esri.jfrog.io/artifactory/arcgis")
+        }
+    }
+}
+
+rootProject.name = "ArcgisAPI"
+include(":app")
+ 
